2d5afa066a48a8603d03cd213b435d5369b522ff
Ivan Grce <ivan.grce@microblink.com>
2021-04-28 14:04:09 +0200

    [NuGet] updated package names, versions and descriptions

--- Binding/Forms/BlinkCard.Forms/BlinkCard.Forms.iOS/Properties/AssemblyInfo.cs
+++ Binding/Forms/BlinkCard.Forms/BlinkCard.Forms.iOS/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("BlinkID.Forms.iOS")]
+[assembly: AssemblyTitle("BlinkCard.Forms.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microblink")]
-[assembly: AssemblyProduct("BlinkID")]
+[assembly: AssemblyProduct("BlinkCard")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
